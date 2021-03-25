5f4491d99342cd723be29993cb62e041fab11dd6
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-25 20:13:53 +0100

    Added RESCAN

--- src/main/java/org/bff/javampd/admin/AdminProperties.java
+++ src/main/java/org/bff/javampd/admin/AdminProperties.java
@@ -7,6 +7,7 @@ public class AdminProperties extends MPDProperties {
     private enum Command {
         KILL("admin.kill"),
         REFRESH("admin.refresh"),
+        RESCAN("admin.rescan"),
         OUTPUTS("admin.outputs"),
         OUTPUT_ENABLE("admin.enable.out"),
         OUTPUT_DISABLE("admin.disable.out");
